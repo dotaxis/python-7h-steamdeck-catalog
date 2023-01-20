c0c65dc0aa5414b0ff75b0ef6a6a1ab1943f3329
Chase Taylor <x8@live.ca>
2023-01-20 04:04:53 -0700

    Modify installer

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sudo pacman -Sy python
+sudo steamos-readonly disable
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 rm get-pip.py
 export PATH="$PATH:/home/deck/.local/bin"
 source ~/.bashrc
 pip install -r requirements.txt
+sudo steamos-readonly enable
