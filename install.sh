2ca8f72e96721610b16ae8ae2bbb0895dd8517fb
Chase Taylor <x8@live.ca>
2023-01-20 00:40:00 -0700

    Added install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo pacman -Sy python
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+rm get-pip.py
+export PATH="$PATH:/home/deck/.local/bin"
+source ~/.bashrc
+pip install -r requirements.txt
